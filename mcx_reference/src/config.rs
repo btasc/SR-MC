0215f37b119bcac1be0ad453d770124efd6497ad
btasc <176724843+btasc@users.noreply.github.com>
2026-07-07 20:27:19 -0400

    Wrote traverse and scatter, debugged sample_path

--- mcx_reference/src/config.rs
+++ mcx_reference/src/config.rs
@@ -27,11 +27,18 @@ impl<T: Copy + Default, const N: usize> Volume<T, N> {
         idx
     }
 
+    #[inline]
+    pub fn in_bounds(&self, coords: [usize; N]) -> bool {
+        (0..N).all(|i| coords[i] < self.dims[i])
+    }
+
     pub fn get(&self, coords: [usize; N]) -> T {
+        debug_assert!(self.in_bounds(coords), "Volume::get was given indices out of bounds. Coords: {:?}. Dimensions: {:?}", coords, self.dims);
         self.data[self.index(coords)]
     }
 
     pub fn get_mut(&mut self, coords: [usize; N]) -> &mut T {
+        debug_assert!(self.in_bounds(coords), "Volume::get_mut was given indices out of bounds. Coords: {:?}. Dimensions: {:?}", coords, self.dims);
         &mut self.data[self.index(coords)]
     }
 }
@@ -40,15 +47,24 @@ pub struct Material {
     pub mu_a: f32,
     pub mu_s: f32,
     pub mu_t: f32,
+    
+    // add henyey greenstein values
+    pub g: f32,
+    pub g_2: f32,
+    pub inv_2g: f32,
 }
 
 pub struct PhotonStateF32 {
-    pub local_pos: Vec3,
+    // position 0 through 1 inside the current voxel
+    pub pos: Vec3,
+    // global voxel coordinate
     pub global_pos: [usize; 3],
+    pub dir: Vec3,
     pub weight: f32,
     pub path: f32,
     pub is_path_done: bool,
     pub time: f32,
     pub voxel_acc: f32,
     pub current_voxel_idx: usize,
+    pub rng: rand::rngs::ThreadRng,
 }
